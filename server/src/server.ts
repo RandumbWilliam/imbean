18d717c5c0e13c7a0ba750fa6c4a11399853c4c0
RandumbWilliam <william.zhen8166@hotmail.com>
2024-07-21 16:26:40 -0400

    organize server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
-import { ValidateEnv } from "./utils/validateEnv";
-import { App } from "./app";
-import { AuthRoute } from "./routes/auth.route";
+import { App } from "@/app";
+import { ValidateEnv } from "@utils/validateEnv";
+
+import { AuthRoute } from "@routes/auth.route";
+import { UserRoute } from "@routes/users.route";
 
 ValidateEnv();
 
-const app = new App([new AuthRoute()]);
+const app = new App([new AuthRoute(), new UserRoute()]);
 
 app.listen();
